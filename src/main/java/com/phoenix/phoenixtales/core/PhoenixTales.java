bb0b75e8da82355ac10b831b6e04e426e620f1f7
WithPhoenix <felix.unger1@outlook.de>
2022-06-26 16:18:27 +0200

    Initial commit

---
+++ src/main/java/com/phoenix/phoenixtales/core/PhoenixTales.java
@@ -0,0 +1,35 @@
+package com.phoenix.phoenixtales.core;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(value = PhoenixTales.MOD_ID)
+public class PhoenixTales {
+
+    public static final String MOD_ID = "phoenixtales";
+    public static final Logger log = LogUtils.getLogger();
+
+
+    public PhoenixTales() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        log.info("loaded");
+    }
+}
